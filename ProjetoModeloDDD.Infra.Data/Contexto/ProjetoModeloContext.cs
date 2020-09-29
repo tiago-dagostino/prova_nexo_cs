07601b7f9f7029c92300a34113f02c359b1128ab
Tiago DAgostino <tiago.dagostino@usp.br>
2020-09-29 15:12:13 -0300

    Add project files.

---
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using ProjetoModeloDDD.Domain.Entities;
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext() : base("ProjetoModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+           modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+           modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+           modelBuilder.Properties()
+               .Where(p => p.Name == p.ReflectedType.Name + "Id")
+               .Configure(p => p.IsKey());
+
+           modelBuilder.Properties<string>()
+               .Configure(p => p.HasColumnType("varchar"));
+
+           modelBuilder.Properties<string>()
+               .Configure(p => p.HasMaxLength(100));
+
+        }
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+
+    }
+}
